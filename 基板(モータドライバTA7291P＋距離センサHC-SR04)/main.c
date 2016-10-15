dd6a3d8deff243a8dc24777710357871ab6d42ef
shigeru yokochi <shigeru@yokochi.jp>
2016-10-15 22:53:41 +0900

    update

--- 基板(モータドライバTA7291P＋距離センサHC-SR04)/main.c
+++ 基板(モータドライバTA7291P＋距離センサHC-SR04)/main.c
@@ -1,29 +1,72 @@
-﻿//gcc main.c -o main -lwiringPi
+﻿//gcc -Wall -O2 main.c -o main -lwiringPi
 #include <stdio.h>
 #include <stdlib.h>
 #include <wiringPi.h>
+#include <unistd.h>
+#include <sys/time.h>
 
 //HC-SR04
-#define    HC_SR04_TRIG_PIN    8
-#define    HC_SR04_ECHO_PIN    7
-int HC_SR04_init(void);
+#define	HC_SR04_TRIG_PIN		8
+#define	HC_SR04_ECHO_PIN		7
+//TA7291P
+#define TA7291P_MOTOR_FORWARD_PIN 	14
+#define TA7291P_MOTOR_REVERSE_PIN 	15
+#define TA7291P_MOTOR_PWM_PIN 	18
+//#define TA7291P_OUT_POWER 		512
+#define TA7291P_STOP			0
+#define TA7291P_FORWARD			1
+#define TA7291P_REVERSE			2
+
+//HC-SR04
+void HC_SR04_init(void);
 long HC_SR04_get_distance(void);
 int HC_SR04_pulseIn(int pin, int level, int timeout);
+//TA7291P
+int m_nTA7291P_mode;
+void TA7291P_init(void);
+void TA7291P_forward(int nPower);
+void TA7291P_reverse(int nPower);
+void TA7291P_stop(void);
 /*****************************************************************
 *
 *****************************************************************/
 int main (int argc, char **argv)
 {
-	int i,j,k,l,m;
+	int i;
 	long distance;
 
-	if(HC_SR04_init() == -1)return 1;	//HC-SR04 初期化
+	if(wiringPiSetupGpio() == -1){
+		printf("wiringPiSetupGpio() error\n");
+		return -1;
+	}
+
+	HC_SR04_init();	//HC-SR04 初期化
+	TA7291P_init();	//TA7291P 初期化
+
 
 	for(i=0;i<200;i++){
 		delayMicroseconds(100000); //wait
 		distance = HC_SR04_get_distance();	//HC-SR04 距離取得
 
-		printf("distance=%d cm\n",distance);
+
+		if(distance >= 20){
+			if(m_nTA7291P_mode != TA7291P_FORWARD){
+				TA7291P_forward(512);	//TA7291P 正回転開始
+			}
+		}
+
+
+		if(distance < 10){
+			TA7291P_stop();	//TA7291P 停止
+		}
+	
+
+		printf("distance=%ld cm\n",distance);
+	}
+
+	TA7291P_stop();	//TA7291P 停止
+
+	return 0;
 }
 
 /*****************************************************************
@@ -50,13 +93,10 @@ long HC_SR04_get_distance(void)
 /*****************************************************************
 *	HC-SR04 初期化
 *****************************************************************/
-int HC_SR04_init(void)
+void HC_SR04_init(void)
 {
-	if(wiringPiSetupGpio() == -1) return -1;
 	pinMode(HC_SR04_TRIG_PIN,OUTPUT);
 	pinMode(HC_SR04_ECHO_PIN,INPUT);
-
-	return 0;
 }
 /*****************************************************************
 *	HC-SR04 パルス波時間を取得する
@@ -70,15 +110,25 @@ int HC_SR04_pulseIn(int pin, int level, int timeout)
 	micros = 0;
 	while (digitalRead(pin) != level){
 		gettimeofday(&tn, NULL);
-		if (tn.tv_sec > t0.tv_sec) micros = 1000000L; else micros = 0;
+		if (tn.tv_sec > t0.tv_sec){
+			micros = 1000000L;
+		}
+		else{
+			micros = 0;
+		}
 		micros += (tn.tv_usec - t0.tv_usec);
 		if (micros > timeout) return 0;
 	}
 
 	gettimeofday(&t1, NULL);
 	while (digitalRead(pin) == level){
-	gettimeofday(&tn, NULL);
-		if (tn.tv_sec > t0.tv_sec) micros = 1000000L; else micros = 0;
+		gettimeofday(&tn, NULL);
+		if (tn.tv_sec > t0.tv_sec){
+			micros = 1000000L;
+		}
+		else{
+			micros = 0;
+		}
 		micros = micros + (tn.tv_usec - t0.tv_usec);
 		if (micros > timeout) return 0;
 	}
@@ -88,3 +138,58 @@ int HC_SR04_pulseIn(int pin, int level, int timeout)
 
 	return micros;
 }
+/*****************************************************************
+*	TA7291P 初期化
+*****************************************************************/
+void TA7291P_init(void)
+{
+	m_nTA7291P_mode = TA7291P_STOP;
+
+	pinMode(TA7291P_MOTOR_FORWARD_PIN, OUTPUT);
+	pinMode(TA7291P_MOTOR_REVERSE_PIN, OUTPUT);
+	pinMode(TA7291P_MOTOR_PWM_PIN, PWM_OUTPUT);
+
+	TA7291P_stop();	//TA7291P 停止
+}
+/*****************************************************************
+*	TA7291P 正回転
+*   nPower:0..　回転速度(0が停止)
+*****************************************************************/
+void TA7291P_forward(int nPower)
+{
+	if(m_nTA7291P_mode == TA7291P_REVERSE){
+		TA7291P_stop();	//TA7291P 停止
+		delayMicroseconds(50000); //wait(50ms)
+	}
+
+	m_nTA7291P_mode = TA7291P_FORWARD;
+	digitalWrite(TA7291P_MOTOR_FORWARD_PIN, HIGH);
+	pwmWrite(TA7291P_MOTOR_PWM_PIN, nPower);
+}
+/*****************************************************************
+*	TA7291P 逆回転
+*   nPower:0..　回転速度(0が停止)
+*****************************************************************/
+void TA7291P_reverse(int nPower)
+{
+	if(m_nTA7291P_mode == TA7291P_FORWARD){
+		TA7291P_stop();	//TA7291P 停止
+		delayMicroseconds(50000); //wait(50ms)
+	}
+
+	m_nTA7291P_mode = TA7291P_REVERSE;
+	digitalWrite(TA7291P_MOTOR_REVERSE_PIN, HIGH);
+	pwmWrite(TA7291P_MOTOR_PWM_PIN, nPower);
+}
+/*****************************************************************
+*	TA7291P 停止
+*****************************************************************/
+void TA7291P_stop(void)
+{
+	m_nTA7291P_mode = TA7291P_STOP;
+	pwmWrite(TA7291P_MOTOR_PWM_PIN, LOW);
+	digitalWrite(TA7291P_MOTOR_FORWARD_PIN, LOW);
+	digitalWrite(TA7291P_MOTOR_REVERSE_PIN, LOW);
+}
+
+
